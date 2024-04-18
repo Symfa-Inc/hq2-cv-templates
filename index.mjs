ba8c04a70338475c48048e6d1c4b358d591a732c
vlad.bairash <uladzislau.bairash@symfa.com>
2024-04-18 11:40:41 +0300

    v1.1.4

--- index.mjs
+++
@@ -1,8 +0,0 @@
-import handlebars from 'handlebars';
-import {template} from './templates/cv-default-template.js';
-
-function getHTMLTemplate() {
-    return handlebars.compile(template);
-}
-
-export {getHTMLTemplate};
