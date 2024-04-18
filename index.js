86d45cf2ccdbaf6641c021058fc6c140edc09751
vlad.bairash <uladzislau.bairash@symfa.com>
2024-04-18 11:29:32 +0300

    v1.1.2

--- index.js
+++
@@ -1,8 +0,0 @@
-const defaultTemplate = require('./templates/cv-default-template.hbs');
-const handlebars = require('handlebars');
-
-function getHTMLTemplate() {
-    return handlebars.compile(defaultTemplate);
-}
-
-module.exports = getHTMLTemplate;
